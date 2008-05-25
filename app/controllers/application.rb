6e399d4e9b054264b1fcc5d2c6c2ac9068d69356
Josh Adams <josh@isotope11.com>
2008-05-25 15:28:52 -0500

    ran community engine setup

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,5 +6,5 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'b550488063deeccc709acc609030c629'
+  protect_from_forgery :secret => 'b550488063deeccc709acc609030c629'
 end
